d700e938a76c5e8273202f15a05e1066c8acef10
Zhehao Chen <zchen2404@MBP-chen.lan>
2024-01-01 21:23:50 -0500

    hw

---
+++ puppypals/src/App.jsx
@@ -0,0 +1,43 @@
+
+import { useState } from 'react'
+import './App.css'
+import {puppyList} from './data.js'
+import './path-to-css.css'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+
+  function handleClick() {
+    
+  }
+
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+
+
+
+  return (
+    <div className="App">
+
+      {puppies.map((puppy) => {
+        return (
+          <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>
+            {puppy.name}
+          </p>
+        );
+      })}
+            {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+
+  )
+}
+
+export default App
